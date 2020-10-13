4f4b5701652fa29fbd9c6c8aa0bb838c210cfa60
dowhatidream <rkfgusch@naver.com>
2020-10-13 10:07:58 +0900

    six

--- flyscanner/urls.py
+++ flyscanner/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
     path('recommender/', include('recommender.urls'))
 ]
